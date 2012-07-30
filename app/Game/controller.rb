09bcd03cca609e8e8063a9bf3b93604b47e27a3c
Ilya Nemihin <nemilya@gmail.com>
2012-07-30 16:59:29 +0600

    first version GameLife

---
+++ app/Game/controller.rb
@@ -0,0 +1,12 @@
+require 'rho'
+require 'rho/rhocontroller'
+require 'rho/rhoerror'
+require 'helpers/browser_helper'
+
+class GameController < Rho::RhoController
+  include BrowserHelper
+  
+  def index
+  end
+
+end
